84578013c7731b37bd4b1a6d09aa323378fc54d6
BaxleyAldworth <jbaxle@gmail.com>
2015-12-14 14:23:45 -0600

    Created post class and dbcontext

---
+++ Week8Lab.Reddit/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Week8Lab.Reddit.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public int Up { get; set; }
+        public int Down { get; set; }
+        [NotMapped]
+        public int Popularity { get { return Up - Down; } }
+        public DateTime PostDate { get; set; }
+    }
+}
---
+++ Week8Lab.Reddit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week8Lab.Reddit.Startup))]
+namespace Week8Lab.Reddit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
